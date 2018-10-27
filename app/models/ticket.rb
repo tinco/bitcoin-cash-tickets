a3fdfe2708df66f553645caaed9aa5159d215e8f
Tinco Andringa <mail@tinco.nl>
2018-10-27 23:21:41 +0200

    implement salt and verify methods

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,11 @@
 class Ticket < ApplicationRecord
   belongs_to :event
+
+  def salt
+    Digest::SHA256.hexdigest(user_info.to_json)[0..12]
+  end
+
+  def verify(salt)
+    self.salt == salt
+  end
 end
